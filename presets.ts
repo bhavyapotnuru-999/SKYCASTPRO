db7b90ee79b260b753bef05a6a826fcc211cd512
Potnuru Bhavya <potnurubhavya999@gmail.com>
2026-06-21 11:24:40 +0530

    added presets.ts

---
+++ presets.ts
@@ -0,0 +1,54 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { LocationData } from "../types";
+
+export const PRESET_CITIES: LocationData[] = [
+  {
+    id: 2643743,
+    name: "London",
+    latitude: 51.50853,
+    longitude: -0.12574,
+    country: "United Kingdom",
+    admin1: "England",
+    timezone: "Europe/London",
+  },
+  {
+    id: 5128581,
+    name: "New York",
+    latitude: 40.71427,
+    longitude: -74.00597,
+    country: "United States",
+    admin1: "New York",
+    timezone: "America/New_York",
+  },
+  {
+    id: 1850147,
+    name: "Tokyo",
+    latitude: 35.6895,
+    longitude: 139.69171,
+    country: "Japan",
+    admin1: "Tokyo",
+    timezone: "Asia/Tokyo",
+  },
+  {
+    id: 2147714,
+    name: "Sydney",
+    latitude: -33.86785,
+    longitude: 151.20732,
+    country: "Australia",
+    admin1: "New South Wales",
+    timezone: "Australia/Sydney",
+  },
+  {
+    id: 2988507,
+    name: "Paris",
+    latitude: 48.85341,
+    longitude: 2.3488,
+    country: "France",
+    admin1: "Île-de-France",
+    timezone: "Europe/Paris",
+  },
+];
